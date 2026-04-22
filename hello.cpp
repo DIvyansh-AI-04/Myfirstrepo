a88ee7d590007506d460cd7699245b39e9927e5b
Divyansh Singh <25LBCS3269@culkomail.in>
2026-04-22 10:44:00 +0530

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    printf("enter a number: ");
+    int n;
+  cin>>n;
+    if (n%2==0)
+    {
+       cout<<"even";
+    } else{
+        cout<<"odd";
+    }
+    
+
+
+    return 0;
+}
